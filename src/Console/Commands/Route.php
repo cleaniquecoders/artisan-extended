982d430ad2a30f04897a1edd3a8c6a9c7cf638b1
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-12-14 06:56:48 +0800

    Remake make:route

--- src/Console/Commands/Route.php
+++ src/Console/Commands/Route.php
@@ -2,16 +2,18 @@
 
 namespace CleaniqueCoders\ArtisanExtended\Console\Commands;
 
-use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
 
-class Route extends Command
+class Route extends GeneratorCommand
 {
     /**
-     * The name and signature of the console command.
+     * The console command name.
      *
      * @var string
      */
-    protected $signature = 'make:route {name} {--api}';
+    protected $name = 'make:route';
 
     /**
      * The console command description.
@@ -21,31 +23,116 @@ class Route extends Command
     protected $description = 'Create a new resourceful route';
 
     /**
-     * Create a new command instance.
+     * The type of class being generated.
      *
-     * @return void
+     * @var string
      */
-    public function __construct()
+    protected $type = 'Route';
+
+    /**
+     * Get the desired class name from the input.
+     *
+     * @return string
+     */
+    protected function getNameInput()
     {
-        parent::__construct();
+        $name = trim($this->argument('name'));
+        $name = ($this->option('p')) ? Str::studly($this->option('p')) . '\\' . $name : $name;
+        return ($this->option('a')) ? 'API\\' . $name : $name;
     }
 
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return bool|null
      */
-    public function handle()
+    public function fire()
     {
-        $name = $this->argument('name');
-        $controller = studly_case(class_basename($name)) . 'Controller';
-        $route_name = str_slug(str_plural($name));
-        $content = 'Route::resource(\'' . $route_name . '\', \'' . $controller . '\');' . PHP_EOL;
+        parent::fire();
+        $name = $this->parseName($this->getNameInput());
+
+        $content = '\\' . $this->getNamespace($name) . '\\' . str_replace($this->getNamespace($name) . '\\', '', $name) . '::routes();' . PHP_EOL;
+
         file_put_contents(
-            base_path('routes/' . ($this->option('api') ? 'api' : 'web') . '.php'),
+            base_path('routes/' . ($this->option('a') ? 'api' : 'web') . '.php'),
             $content,
             FILE_APPEND
         );
-        $this->info('Route created successfully.');
+    }
+
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        return str_replace([
+            'DummyClass',
+            'DummyPrefix',
+            'DummyMiddleware',
+            'DummyRoute',
+        ], [
+            $this->getClassName($name),
+            $this->option('p') ?: '',
+            $this->getMiddlewares(),
+            $this->getRouteName(),
+        ], $stub);
+    }
+
+    protected function getClassName($name)
+    {
+        return str_replace($this->getNamespace($name) . '\\', '', $name);
+    }
+
+    protected function getRouteName()
+    {
+        return Str::plural(Str::slug($this->argument('name')));
+    }
+
+    protected function getMiddlewares()
+    {
+        if (empty($this->option('m'))) {
+            return '';
+        }
+
+        return "'" . str_replace(",", "','", $this->option('m')) . "'";
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__ . '/stubs/route.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Routes';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['a', 'a', InputOption::VALUE_NONE, 'Create a new API route.'],
+            ['p', 'p', InputOption::VALUE_OPTIONAL, 'Set prefix for the given route.'],
+            ['m', 'm', InputOption::VALUE_OPTIONAL, 'Set middlewares for the given route.'],
+        ];
     }
 }
